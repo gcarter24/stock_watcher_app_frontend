e3f2321b7ed18cc52e2965c27746ee8ec486d81d
George <gchriscarter@gmail.com>
2021-09-13 18:07:31 -0400

    retrieve name and symbol data from backend and have it display on frontend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,39 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="stock in stocks" v-bind:key="stock.id">
+      <p>Name: {{ stock.name }}</p>
+      <p>Symbol: {{ stock.symbol }}</p>
+      <hr />
+    </div>
+    <button v-on:click="searchStock()">Search</button>
+    <!-- <p>{{ stock.name }}</p> -->
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      id: "",
+      symbol: "",
+      name: "",
+      stocks: {},
     };
   },
   created: function () {},
-  methods: {},
+  methods: {
+    searchStock: function () {
+      // var params = { symbol: this.symbol, name: this.name, id: this.id };
+      axios.get("/api/stocks/").then((response) => {
+        console.log(response.data);
+        this.stocks = response.data;
+      });
+    },
+  },
 };
 </script>
