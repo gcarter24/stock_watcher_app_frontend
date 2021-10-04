09885fe41036e01fb1198fa50a104276d1d9e89f
George <gchriscarter@gmail.com>
2021-10-04 16:12:21 -0400

    add show and modal function to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,21 @@
     <input type="text" v-model="search" placeholder="Search Stocks" />
     <div v-for="stock in filteredStocks" v-bind:key="stock.id">
       <p>Name: {{ stock.name }}</p>
-      <p>Symbol: {{ stock.symbol }}</p>
+      <p>NASDAQ Symbol: {{ stock.symbol }}</p>
+      <p><button v-on:click="stocksShow(stock)">View History</button></p>
       <hr />
     </div>
+    <dialog id="stock-details">
+      <form method="dialog">
+        <p>Name: {{ currentStock.name }}</p>
+        <p>Symbol: {{ currentStock.symbol }}</p>
+        <p>Industry: {{ currentStock.industry }}</p>
+        <p>Sector: {{ currentStock.sector }}</p>
+        <p>IPO Year: {{ currentStock.ipo_year }}</p>
+        <p>Country: {{ currentStock.country }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
     <!-- <button v-on:click="filteredStocks()">Search</button> -->
     <!-- <p>{{ stock.name }}</p> -->
   </div>
@@ -16,15 +28,17 @@
 
 <script>
 import axios from "axios";
+// import ApexCharts from "apexcharts";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "NASDAQ Stock Market Watcher",
       id: "",
       symbol: "",
       name: "",
       search: "",
       stocks: [],
+      currentStock: {},
     };
   },
   created: function () {
@@ -42,6 +56,11 @@ export default {
     //   this.stocks = response.data;
     // });
     // },
+    stocksShow: function (theStock) {
+      console.log(theStock);
+      this.currentStock = theStock;
+      document.querySelector("#stock-details").showModal();
+    },
   },
   computed: {
     filteredStocks: function () {
